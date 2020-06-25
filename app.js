40afe53c90d07edb7d9f108f8957faa2f905629b
EFdez <escarlafdez@gmail.com>
2020-06-26 00:17:35 +0200

    TODO fix movie detail

--- app.js
+++ app.js
@@ -37,5 +37,7 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 const index = require('./routes/index');
 app.use('/', index);
+app.use('/movies', index);
+app.use('/movies/movie/:id', index);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -12,13 +12,29 @@ router.get('/movies', (req, res, next) => {
         .catch(err => console.log("Error en la BBDD", err))
 })
 
-router.get('/movie/:id', (req, res) => {
+// router.get('/movie/:id', (req, res, next) => { //ASÍ SÍ SALE.
+//     const id = req.params.id
+//     Movie.findById('5ef4f26fc74ce91f10c99e68')
+//         .then(theMovie => (res.render('movie'), theMovie))
+// })
 
-    Movie.findById(req.params.id)
-        .then(theMovie => res.render('movies/movie', theMovie))
-        .catch(err => console.log("Error en la movie DETALLES", err))
+router.get('/movie/:id', (req, res, next) => { //ASÍ SÍ SALE.
+    const id = req.params.id
+    Movie.findOne({ id: "id" })
+        //.then(theMovie => (res.send(id), theMovie))
+        .then(theMovie => res.render('movie', theMovie))
 })
 
 
+// router.get('/movie/:id', (req, res) => {
+//     const id = req.params.id
+//     id.toString()
+//     Movie.findById(id) //PROBLEMA CON EL ID
+//         //.then(res.send("el id es", id))
+//         .then(theMovie => res.render('movies/movie', theMovie))
+//         .catch(err => console.log("Error en la movie DETALLES", err))
+// })
+
+//5ef4f26fc74ce91f10c99e68
 
 module.exports = router;
